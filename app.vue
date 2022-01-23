94556e0d90853a6e86bef0001a7301d9ca1f65d6
shuntaro-nanao <shuntaro.nno0319@gmail.com>
2022-01-24 00:01:43 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const title = "Hello Nuxt3!!";
+
+useMeta({
+  meta: [{ name: "description", content: `This is ${title} page` }],
+});
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
